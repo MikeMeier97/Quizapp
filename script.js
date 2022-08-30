1527a7170674e60be6f7b919ba40068f1bad56ee
MikeMeier97 <m0.0m58@yahoo.de>
2022-08-30 13:52:40 +0200

    button effekts

---
+++ script.js
@@ -0,0 +1,8 @@
+function answer(i) {
+    if (i == true) {
+        let index = document.getElementById('trueAnswer');+
+        index.classList.add('text-bg-success');
+    } else {
+        alert("Leider falsch!");
+    }
+}
