09c9dc05a114f4363ce7db5af08a29be5ac79b44
MikeMeier97 <m0.0m58@yahoo.de>
2022-09-07 13:39:16 +0200

    answer Buttons

--- script.js
+++ script.js
@@ -40,18 +40,22 @@ let currentQuestion = 0;
 
 function onload() {
     init();
-    loadCurrentQuestion();
+    showQuestion();
     loadPointBase();
     loadPages();
     
 }
 
-function loadCurrentQuestion() {
+function showQuestion() {
     let question = questions[currentQuestion];
+    document.getElementById('questionTitle').innerHTML = question['question'];
+    document.getElementById('answer1').innerHTML = question['answer1'];
+    document.getElementById('answer2').innerHTML = question['answer2'];
+    document.getElementById('answer3').innerHTML = question['answer3'];
+    document.getElementById('answer4').innerHTML = question['answer4'];
 }
 
 function init() {
-
 }
 
 function answer(i) {
