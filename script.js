75b210aa83b9901bdbdd87599deed40dfc75af58
MikeMeier97 <m0.0m58@yahoo.de>
2022-09-07 13:10:38 +0200

    overwork

--- script.js
+++ script.js
@@ -1,26 +1,57 @@
+let questions = [
+{   "question": "Wie alt bin ich?",
+    "answer1": "16",
+    "answer2": "22",
+    "answer3": "25",
+    "answer4": "18",
+    "right_answer": 3
+},
+{   "question": "Als was arbeite ich aktuell?",
+    "answer1": "Weichenwärter",
+    "answer2": "Lokführer",
+    "answer3": "Tranzportleitung",
+    "answer4": "Fahrdienstleiter",
+    "right_answer": 4
+},
+{   "question": "Was esse ich am liebsten?",
+    "answer1": "Pizza",
+    "answer2": "Gemüße",
+    "answer3": "Sushi",
+    "answer4": "Burger",
+    "right_answer": 1
+},
+{   "question": "Wie groß bin ich?",
+    "answer1": "1,72",
+    "answer2": "1,69",
+    "answer3": "1,78",
+    "answer4": "1,82",
+    "right_answer": 4
+},
+{   "question": "Was ist mein Hobby?",
+    "answer1": "Atmen",
+    "answer2": "Zocken",
+    "answer3": "Programmieren",
+    "answer4": "Arbeiten",
+    "right_answer": 3
+}
+];
 let points = 0; 
 let pages = 1; 
 
 function onload() {
+    init();
     loadFirstQuestions();
     loadPointBase();
     loadPages();
     
 }
 
+function init() {
+
+}
+
 function answer(i) {
-    if (i == true) {
-        points = points + 5;
-        let index = document.getElementById('trueAnswer');+
-        index.classList.add('text-bg-success');
-        loadPointBase();
-    }
-    if (i == 0) {
-        let wrong = document.getElementById('wrong1');
-        wrong.classList.add('text-bg-danger');
-        let index = document.getElementById('trueAnswer');+
-        index.classList.add('text-bg-success');
-    }
+    
 }
 
 function loadPointBase(){
@@ -29,9 +60,11 @@ function loadPointBase(){
     pointBase.innerHTML += `${points} von 25 Punkte`
 }
 
+
 function loadPages() {
-    let pagesPoint = document.getElementById('pages');
-    pagesPoint.innerHTML = ``;
-    pagesPoint.innerHTML = `<b>${pages}</b> von <b>5</b>`
+    let pagesCurrently = document.getElementById('pagesCurrently');
+    let pagesAll = document.getElementById('pagesAll');
+    pagesCurrently.innerHTML = `${pages}`;
+    pagesAll.innerHTML = `${questions.length}`;
 }
 
