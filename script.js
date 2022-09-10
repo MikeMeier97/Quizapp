fe0b1be770f3a893c9eb336b9f40391058151a7f
MikeMeier97 <m0.0m58@yahoo.de>
2022-09-10 21:51:42 +0200

    code Cleanup

--- script.js
+++ script.js
@@ -1,46 +1,8 @@
-let questions = [
-{   "question": "Wie alt bin ich?",
-    "answer1": "16",
-    "answer2": "22",
-    "answer3": "25",
-    "answer4": "18",
-    "right_answer": 3
-},
-{   "question": "Als was arbeite ich aktuell?",
-    "answer1": "Weichenwärter",
-    "answer2": "Lokführer",
-    "answer3": "Tranzportleitung",
-    "answer4": "Fahrdienstleiter",
-    "right_answer": 4
-},
-{   "question": "Was esse ich am liebsten?",
-    "answer1": "Pizza",
-    "answer2": "Gemüße",
-    "answer3": "Sushi",
-    "answer4": "Burger",
-    "right_answer": 1
-},
-{   "question": "Wie groß bin ich?",
-    "answer1": "1,72",
-    "answer2": "1,69",
-    "answer3": "1,78",
-    "answer4": "1,82",
-    "right_answer": 4
-},
-{   "question": "Was ist mein Hobby?",
-    "answer1": "Atmen",
-    "answer2": "Zocken",
-    "answer3": "Programmieren",
-    "answer4": "Arbeiten",
-    "right_answer": 3
-}
-];
 let points = 0;  
 let currentQuestion = 0;
 let localQuestionBlock = 0;
 
 function onload() {
-    init();
     showQuestion();
     loadPointBase();
     loadPages();
@@ -56,8 +18,6 @@ function showQuestion() {
     document.getElementById('answer4').innerHTML = question['answer4'];
 }
 
-function init() {
-}
 function answer(selection) {
     let question = questions[currentQuestion];
     let theRightAnswer = 'answer' + question['right_answer'];
@@ -93,3 +53,41 @@ function addPoints () {
         loadPointBase();
     }
 }
+
+let questions = [
+    {   "question": "Wie alt bin ich?",
+        "answer1": "16",
+        "answer2": "22",
+        "answer3": "25",
+        "answer4": "18",
+        "right_answer": 3
+    },
+    {   "question": "Als was arbeite ich aktuell?",
+        "answer1": "Weichenwärter",
+        "answer2": "Lokführer",
+        "answer3": "Tranzportleitung",
+        "answer4": "Fahrdienstleiter",
+        "right_answer": 4
+    },
+    {   "question": "Was esse ich am liebsten?",
+        "answer1": "Pizza",
+        "answer2": "Gemüße",
+        "answer3": "Sushi",
+        "answer4": "Burger",
+        "right_answer": 1
+    },
+    {   "question": "Wie groß bin ich?",
+        "answer1": "1,72",
+        "answer2": "1,69",
+        "answer3": "1,78",
+        "answer4": "1,82",
+        "right_answer": 4
+    },
+    {   "question": "Was ist mein Hobby?",
+        "answer1": "Atmen",
+        "answer2": "Zocken",
+        "answer3": "Programmieren",
+        "answer4": "Arbeiten",
+        "right_answer": 3
+    }
+    ];
